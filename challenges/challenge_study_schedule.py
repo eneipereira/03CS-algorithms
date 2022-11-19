1d7e4dc63d8f54fd723f981a8f71b5cac2930e38
Enei <eneipereira@outlook.com.br>
2022-11-19 01:05:43 -0300

    feat: Implementa uma função que, dada uma lista de horarios de entrada e saida e um horário especifico, retorna a quantidade de pessoas estudantes estavam estudando naquele horário.

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(target_time, int):
+        return None
+
+    students_per_time = 0
+
+    for start, end in permanence_period:
+        if not isinstance(start, int) or not isinstance(end, int):
+            return None
+
+        if start <= target_time <= end:
+            students_per_time += 1
+    
+    return students_per_time
+        
