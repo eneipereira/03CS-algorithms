b2cba1becafc2d5d89d5b186372a38877e88c6a3
Enei <eneipereira@outlook.com.br>
2022-11-19 02:49:12 -0300

    test: Implementa testes para uma função de criptografia

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from pytest import raises
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("AABBCC", 3) == "BAA_CCB"
+    assert encrypt_message("ABBCCA", 4) == "AC_CBBA"
+    assert encrypt_message("AABBCC", -1) == "CCBBAA"
+    
+    with raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("AABBCC", "invalid_key")
+
+    with raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(1234567890, 5)
